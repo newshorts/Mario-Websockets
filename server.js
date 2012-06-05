7263551626a3194265dd312270734e62d6b73762
Michael Newell <mike@iwearshorts.com>
2012-06-05 00:23:57 -0700

    first major update

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
-var express = require('express');
+var App = function() {
+    this.setHeartbeat = function(s) {
+        var s = s;
+        setInterval(function() {
+            s.emit('hi', {connection: 'listening for heartbeat'});
+        }, 2000);
+    }
+}
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
+var fs = require('fs'),
+    express = require('express'),
+    app = express.createServer(express.logger()),
+    io = require('socket.io').listen(app);
+    
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
+});
+    
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/iphone', function(req, res) {
+    res.sendfile(__dirname + '/public/iphone.html');
+});
+
+var a = new App();
+io.sockets.on('connection', function(socket) {
+    
+//    a.setHeartbeat(socket);
+    
+//    socket.emit('hi', { connection: 'trying to establish'});
+    
+    socket.on('hi back', function(data) {
+        console.log(data);
+    });
+    
+    socket.on('button', function(data) {
+        console.log(data);
+        socket.broadcast.emit('button', data)
+    });
+    
 });
